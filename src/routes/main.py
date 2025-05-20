321e845172e1af167330c2d661a49441ae8d1886
OvertimeCap <overtimecap@gmail.com>
2025-05-19 22:03:15 -0300

    Delete src/routes/main.py

--- src/routes/main.py
+++
@@ -1,4 +0,0 @@
-@main_bp.route("/clientes_finalizados/contrato_pronto/<int:cliente_id>")
-@login_required
-def contrato_pronto(cliente_id):
-    return render_template("contrato_pronto.html", cliente_id=cliente_id)
